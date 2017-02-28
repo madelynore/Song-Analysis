f94ca995ca1475a82c5e5f870e0ca2cdb2a60a47
Madelyn <madelynore@gmail.com>
2017-02-28 14:25:23 -0800

    Changed format of Column arrangements, rearranged code better.

--- R_scripts/data_cleaning.r
+++ R_scripts/data_cleaning.r
@@ -11,38 +11,39 @@ library(purrr)
 library(stringr)
 
 # Macauley Library --------------------------------------------------------
-
-
 ML_data <- read_excel("data_raw/ML_SongRecordings.xlsx", sheet=1, col_names=TRUE)
 names(ML_data) <- make.names(names(ML_data))
 #View(ML_data)
+
 
 #Remove unnecessary columns
 NVM <- c(2:4, 6:7, 19, 22:35, 37:38, 40:45,47) #specify columns to eliminate
 ML_col_cleaned <- ML_data %>% 
   select(-NVM)
 
+
 #rearrange columns & change names
-ML_arrange <- ML_col_cleaned[,c(1,3,4,5,6,7,9,8,10,11,12,13,14,15,16,2,18,19,17)] 
-ML_name <- rename(ML_arrange, Remarks = Public.Note, Songtype = Behavior, Processed.comments=Processed.) # new name = old name
+ML_arrange <- ML_col_cleaned[,c("ML.Catalog..","Year","Month","Day","Time","Country","County","State","Locality","Latitude","Longitude","Elevation..m.", "Behavior","Stimulus","Habitat","Background.Species","Processed.","X..of.songs.in.recording", "Public.Note")] 
+ML_name <- rename(ML_arrange, Remarks = Public.Note, Songtype = Behavior, Processed.comments=Processed., Songs.In.Recording = X..of.songs.in.recording) # new name = old name
 #View(ML_name)
 
 
 #separate Database type from ID number
 ML_ID_fix <- ML_name %>% 
   separate(ML.Catalog.., into=c("Database.type" ,"ID"), sep=2) #separates the first two letters
-View(ML_ID_fix)
+#View(ML_ID_fix)
 
-#edit times
-ML_time_fix <- ML_ID_fix %>% 
-  for (x in Time) {
-    if (x==0){
-     x <- is.na(x)
-    } else{
-      hm(x)
-    }
-  }
-View(ML_time_fix)
+
+# #edit times
+# ML_time_fix <- ML_ID_fix$Time %>% 
+#   for (x in Time) {
+#     if (x==0){
+#      x <- is.na(x)
+#     } else{
+#       hm(x)
+#     }
+#   }
+# View(ML_time_fix)
 
 #write ML spreadsheet
 write_csv(ML_ID_fix, path= "data/ML_song_recordings.csv")
@@ -55,26 +56,10 @@ names(XC_data) <- make.names(names(XC_data))
 #View(XC_data)
 
 
-#remove unnecessary columns 
-bye <- c(1:4,16:19)
-XC_data_colRemoved <- XC_data %>% 
-  select(-bye)
-View(XC_data_colRemoved)
-
-#rearrange columns & change names
-XC_renamed <-XC_data_colRemoved %>% 
-  rename(Background.Species = Background, ID = Catalogue.number)  # new name = old name
-XC_addcol <- XC_renamed %>% 
-  mutate(Database.type="XC")
-XC_arrange <- XC_addcol[,c(14,11,1,2,4,3,5,6,7,8,10,12,13,9)]
-
-  # add_column(Database.type= 1:106, .before=1)
-  
-View(XC_arrange)
-
 #Separate columns of date
-XC_date_sep <- XC_arrange %>% 
+XC_date_sep <- XC_data %>% 
   separate(Date, into = c("Year","Month","Day"),sep="-")
+
 
 #clean up Time 
 XC_time_fix <- XC_date_sep %>% 
@@ -82,24 +67,34 @@ XC_time_fix <- XC_date_sep %>%
   #remove the date thing
   select(-wtf)        
 
-View(XC_time_fix)
+#View(XC_time_fix)
 
 #separate state from locality
-# XC_loc<- XC_time_fix %>%
-#   separate(Location, into=c("Locality","State"), extra = 'merge', fill="left")
+XC_loc_fix <- XC_time_fix %>% 
+  mutate(split_location = str_split(Location, ","),
+         State = map_chr(split_location, ~.x[length(.x)])) %>% 
+  # separate(Location, into=c("Locality","strip"), extra = 'merge', fill="right")
 # # XC_loc1 <- X
 # #   separate(State, into=c("County","State"),sep=',',extra="merge", fill='right'C_loc %>% )
 
 
-XC_loc_fix <- XC_time_fix %>% 
-  mutate(split_location = str_split(Location, ","),
-         State = map_chr(split_location, ~.x[length(.x)]))
+#remove unnecessary columns 
+bye <- c(1:4,18:21,24)
+XC_data_colRemoved <- XC_loc_fix %>% 
+  select(-bye)
+View(XC_data_colRemoved)
 
 
-View(XC_loc_fix)
-# 
+#rearrange columns & change names
+XC_renamed <-XC_data_colRemoved %>% 
+  rename(Background.Species = Background, ID = Catalogue.number, Locality = Location, Songs.In.Recording = X..of.songs.in.recording)  # new name = old name
+XC_addcol <- XC_renamed %>% 
+  mutate(Database.type="XC")
+XC_arrange <- XC_addcol[,c("Database.type","ID","Year","Month","Day","Time","Country","State","Locality","Latitude","Longitude","Elevation","Songtype","Background.Species","Process.comments","Songs.In.Recording","Remarks")]
+
+
 # #write XC spreadsheet
-# write_csv(XC_time_fix, path= "data/XC_song_recordings.csv")
+write_csv(XC_arrange, path= "data/XC_song_recordings.csv")
 
 
   
