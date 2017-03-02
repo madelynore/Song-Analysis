8a879385ad9aef45b0e90bc216ed8ecb36ee0784
Madelyn <madelynore@gmail.com>
2017-03-02 13:24:56 -0800

    fixed code explanantions

--- R_scripts/02_assertions.r
+++ R_scripts/02_assertions.r
@@ -10,22 +10,43 @@ XC_data <- read.csv("data/XC_song_recordings.csv")
 
 
 # Macauley Library assertions ---------------------------------------------
+#all states where breeding Townsend's Warblers would be
 states <- c("Alaska", "California", "Montana", "Oregon", "Washington", "Alberta", "British Columbia")
+#nickname of database
 MLname_of_database <- c("ML")
 
 ML_data %>% 
+  #verifys there are the correct number of columns
   verify(ncol(.)==20) %>% 
+  
+  #checks that it is the correct database being tested
   assert(in_set(MLname_of_database), Database.type)
+
+  #checks that the location is a factor
   verify(is.factor(Locality)) %>% 
+    
+  #checks there is a reasonable number of songs in the recording, no misplaced zeros
   assert(within_bounds(0,100), Songs.In.Recording) %>% 
+    
+  #checks that the location of the recordings makes sense
   assert(in_set(states),  State)
 
 # Xeno Canto assertions ---------------------------------------------------
+#nickname of database
 XCname_of_database <- c("XC")
   
 XC_data %>% 
+  #verifys there are the correct number of columns
     verify(ncol(.)==17) %>% 
+  
+  #checks that it is the correct database being tested
     assert(in_set(name_of_database), Database.type)
+
+  #checks that the location is a factor
   verify(is.factor(Locality)) %>% 
+    
+    #checks there is a reasonable number of songs in the recording, no misplaced zeros
     assert(within_bounds(0,100), Songs.In.Recording) %>% 
+    
+    #checks that the location of the recordings makes sense
     assert(in_set(states),  State)
