2417f7edbd2be86923a253c1fc45f316f057598d
Madelyn <madelynore@gmail.com>
2017-03-02 13:31:26 -0800

    fixed problem with XC file path

--- R_scripts/03_metadata_writing.r
+++ R_scripts/03_metadata_writing.r
@@ -12,6 +12,6 @@ read.csv("data/ML_metadata.csvy", comment.char = "#")  #reads in the data, ignor
 
 write_csvy(XC_data, file= "data/XC_metadata.csvy")#writes out the levels of the data
 
-read.csv("data/MLmetadata.csvy", comment.char = "#")#reads in the data, ignore all the comments and just shows the csv
+read.csv("data/XC_metadata.csvy", comment.char = "#")#reads in the data, ignore all the comments and just shows the csv
 
 
