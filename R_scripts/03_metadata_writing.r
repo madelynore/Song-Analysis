21052b79dc1b81a9c69a0c3a4310378bf5e095b7
Madelyn <madelynore@gmail.com>
2017-03-02 13:26:05 -0800

    edited comments

--- R_scripts/03_metadata_writing.r
+++ R_scripts/03_metadata_writing.r
@@ -5,10 +5,13 @@ library(csvy)
 library(dplyr)
 library(readr)
 
-write_csvy(ML_data, file= "data/ML_metadata.csvy") #writes out the levels of the data
-read.csv("data/ML_metadata.csvy", comment.char = "#")  #ignore all the comments and just shows the csv
 
-write_csvy(XC_data, file= "data/XC_metadata.csvy")
-read.csv("data/MLmetadata.csvy", comment.char = "#")
+write_csvy(ML_data, file= "data/ML_metadata.csvy") #writes out the levels of the data
+
+read.csv("data/ML_metadata.csvy", comment.char = "#")  #reads in the data, ignore all the comments and just shows the csv
+
+write_csvy(XC_data, file= "data/XC_metadata.csvy")#writes out the levels of the data
+
+read.csv("data/MLmetadata.csvy", comment.char = "#")#reads in the data, ignore all the comments and just shows the csv
 
 
